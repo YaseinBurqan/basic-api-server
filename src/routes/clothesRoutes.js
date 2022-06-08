ec3e0812f6c66ea96a5aa26d2f88edcd0b94a6f9
Yasein Burqan <mr.yasein@gmail.com>
2022-06-09 02:37:40 +0300

    basic-api-server

--- src/routes/clothesRoutes.js
+++ src/routes/clothesRoutes.js
@@ -1,37 +1,43 @@
 "use strict";
 const express = require("express");
+const app = express();
 
-const { Clothes } = require("../models/index");
+const { clothes } = require("../models/index");
 
 const ClothesRouter = express.Router();
 
+ClothesRouter.get("/", handelhome);
 ClothesRouter.get("/clothes", getClothes);
 ClothesRouter.get("/clothes/:id", getOneClothes);
 ClothesRouter.post("/clothes", createClothes);
 ClothesRouter.put("/clothes/:id", updateClothes);
 ClothesRouter.delete("/clothes/:id", deleteClothes);
 
+function handelhome(req, res) {
+  res.status(200).send("Hello User");
+}
+
 async function getClothes(req, res) {
-  const allClothes = await Clothes.findAll();
+  const allClothes = await clothes.findAll();
   res.status(200).json(allClothes);
 }
 
 async function getOneClothes(req, res) {
   const clothesId = parseInt(req.params.id);
-  let clothesName = await Clothes.findOne({ where: { id: clothesId } });
+  let clothesName = await clothes.findOne({ where: { id: clothesId } });
   res.status(200).json(clothesName);
 }
 
 async function createClothes(req, res) {
   let newClothes = req.body;
-  let person = await Clothes.create(newClothes);
+  let person = await clothes.create(newClothes);
   res.status(201).json(person);
 }
 
 async function updateClothes(req, res) {
   let clothesId = parseInt(req.params.id);
   let updateClothes = req.body;
-  let foundClothes = await Clothes.findOne({ where: { id: clothesId } });
+  let foundClothes = await clothes.findOne({ where: { id: clothesId } });
   if (foundClothes) {
     let updatedClothes = await foundClothes.update(updateClothes);
     res.status(201).json(updatedClothes);
@@ -42,7 +48,7 @@ async function updateClothes(req, res) {
 
 async function deleteClothes(req, res) {
   let clothesId = parseInt(req.params.id);
-  let deleteClothes = await Clothes.destroy({ where: { id: clothesId } });
+  let deleteClothes = await clothes.destroy({ where: { id: clothesId } });
   res.status(204).json(deleteClothes);
 }
 
