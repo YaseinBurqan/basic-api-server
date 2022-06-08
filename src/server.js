970feedd7965171fee8d7ce61818075373b6a6b2
Yasein Burqan <mr.yasein@gmail.com>
2022-06-09 00:08:12 +0300

    basic-api-server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 "use strict";
 require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const express = require("express");
 const app = express();
